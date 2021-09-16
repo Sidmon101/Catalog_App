c2299b1f3de33958f4b7fbb60e469824449f8602
Sidmon101 <sidharthmohanaggarwal@gmail.com>
2021-09-16 14:29:25 +0530

    day 6 completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -3,9 +3,16 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utility/routes.dart';
 
-class loginpage extends StatelessWidget {
+class loginpage extends StatefulWidget {
   const loginpage({Key? key}) : super(key: key);
 
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  String name = "";
+  bool changebutton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +25,7 @@ class loginpage extends StatelessWidget {
                 child: Text("Powered by Sidmon"),
               ),
               Text(
-                "Welcome Shopper!",
+                "Welcome Shopper $name!",
                 style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
@@ -44,6 +51,15 @@ class loginpage extends StatelessWidget {
                         hintText: "Enter your Name",
                         labelText: "USERNAME",
                       ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(
+                          () {},
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
                     ),
                     TextFormField(
                       obscureText: true,
@@ -55,13 +71,48 @@ class loginpage extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.homeroute);
-                        },
-                        child: Text("Login"),
-                        style:
-                            TextButton.styleFrom(minimumSize: Size(100, 40))),
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changebutton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeroute);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changebutton ? 65 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changebutton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            // shape: changebutton
+                            //    ? BoxShape.circle
+                            //  : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changebutton ? 60 : 8)),
+                      ),
+                    )
+                    //  ElevatedButton(
+                    //    onPressed: () {
+                    //      Navigator.pushNamed(context, MyRoutes.homeroute);
+                    //    },
+                    //  child: Text("Login"),
+                    //   style:
+                    //       TextButton.styleFrom(minimumSize: Size(100, 40))),
                   ],
                 ),
               )
